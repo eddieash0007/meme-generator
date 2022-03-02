c23b9e649c05bfa6a9eb88af1317fee9cc8a8ec7
Eddie Ash <edwinashie289@outlook.com>
2022-03-02 17:10:03 +0000

    use effect implemented

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -11,10 +11,19 @@ export default function Meme() {
             randomImage: "http://i.imgflip.com/1bij.jpg"
         })
 
+        const [allMemes, setAllMemes] = React.useState([])
+
+        React.useEffect(()=>{
+                fetch("https://api.imgflip.com/get_memes")
+                .then(res => res.json())
+                .then(data => setAllMemes(data.data.memes))
+            }, [])
+
+            
         function getMemeimage(){
-            const memesArray = memes.data.memes
-            const randomNumber = Math.floor(Math.random() * memesArray.length)
-            const url = memesArray[randomNumber].url
+            
+            const randomNumber = Math.floor(Math.random() * allMemes.length)
+            const url = allMemes[randomNumber].url
             setMeme(prevMeme => ({
                 ...prevMeme,
                 randomImage: url
